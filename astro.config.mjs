748654c29cb28fe251ea7414d3824a0b8732be90
tadzyla <tadas.zvinklys@gmail.com>
2022-03-18 18:48:28 +0000

    Setting up project with astro

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	// Set "renderers" to "[]" to disable all default, builtin component support.
+	renderers: [],
+	vite: {
+		ssr: {
+			external: ["svgo"],
+		},
+	},
+});
