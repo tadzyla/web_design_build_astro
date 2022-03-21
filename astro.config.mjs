349ca77a7177a8d58f5b210e04710abb8bbdc2c8
tadzyla <tadas.zvinklys@gmail.com>
2022-03-21 14:07:06 +0000

    Adding Navbar component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	// Set "renderers" to "[]" to disable all default, builtin component support.
 	renderers: [],
 	vite: {
 		ssr: {
